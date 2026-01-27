26daa32222a09374c4054fcbc96ab9a2a7feb4e1
Arifin Firdaus (Work) <arifin@ictechnology.com.au>
2026-01-27 09:32:07 +0700

    Initial Commit

---
+++ IosBaseProject/IosBaseProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosBaseProjectApp.swift
+//  IosBaseProject
+//
+//  Created by arifin-ict on 27/01/26.
+//
+
+import SwiftUI
+
+@main
+struct IosBaseProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IosBaseProjectTests/IosBaseProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  IosBaseProjectTests.swift
+//  IosBaseProjectTests
+//
+//  Created by arifin-ict on 27/01/26.
+//
+
+import Testing
+@testable import IosBaseProject
+
+struct IosBaseProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
