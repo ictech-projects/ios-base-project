3737a80046a7fe5643e7873140e28969e08c5b7d
aguspranata01 <agus@ictechnology.com.au>
2026-01-28 17:40:13 +0700

    Add labeled picker item component and localizable

---
+++ IosBaseProject/Common/Components/Picker/SingleItemPicker/SingleItemPicker.swift
@@ -0,0 +1,92 @@
+//
+//  SinglePickerItem.swift
+//  IosBaseProject
+//
+//  Created by Ewide Dev 5 on 28/01/26.
+//
+
+import SwiftUI
+
+struct SingleItemPicker<Item, Content>: View where Item: Hashable, Content: View {
+	@Binding var selection: Item?
+	let title: String?
+	let content: () -> Content
+	
+	init(selection: Binding<Item?>, title: String? = nil, content: @escaping () -> Content) {
+		self._selection = selection
+		self.title = title
+		self.content = content
+	}
+	
+	var body: some View {
+		VStack(alignment: .leading) {
+			if let title = title {
+				Text(title)
+					.font(.baseStyle(size: 16, weight: .medium))
+					.foregroundStyle(.neutral90)
+			}
+			
+			Menu {
+				Picker("", selection: $selection, content: content)
+			} label: {
+				HStack {
+					Group {
+						if let item = selection as? String {
+							Text(item)
+								.foregroundStyle(.neutral90)
+						} else {
+							Text("Choose types of leave")
+								.foregroundStyle(.neutral60)
+						}
+					}
+					.lineLimit(1)
+					.font(.baseStyle(size: 12, weight: .regular))
+					
+					Spacer()
+					
+					Image(.chevronDownIcon)
+						.renderingMode(.template)
+						.resizable()
+						.scaledToFit()
+						.frame(width: 20)
+						.foregroundStyle(selection != nil ? .neutral90 : .neutral50)
+				}
+				.frame(height: 46)
+				.padding(.horizontal)
+				.overlay {
+					RoundedRectangle(cornerRadius: 12)
+						.stroke(selection != nil ? .neutral90 : .neutral50, lineWidth: 1)
+				}
+				.contentShape(RoundedRectangle(cornerRadius: 12))
+			}
+		}
+	}
+}
+
+#Preview("Without Label") {
+	PreviewItemPicker()
+}
+
+#Preview("With Labeled") {
+	PreviewItemPicker(title: "Type of leave")
+}
+
+fileprivate struct PreviewItemPicker: View {
+	@State var selection: String?
+	@State var displaySelection = ""
+	
+	private let title: String?
+	
+	init(title: String? = nil) {
+		self.title = title
+	}
+	
+	var body: some View {
+		SingleItemPicker(selection: $selection, title: title) {
+			ForEach(0...5, id: \.self) { item in
+				Text("item \(item)")
+					.tag("item \(item)")
+			}
+		}
+	}
+}
