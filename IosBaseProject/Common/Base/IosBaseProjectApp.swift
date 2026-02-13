eaa46e34ecd817231abf7fb1fc663d5665c5fe9c
Arifin Firdaus (Work) <arifin@ictechnology.com.au>
2026-02-13 09:58:50 +0700

    Add push notification code presetup

--- IosBaseProject/Common/Base/IosBaseProjectApp.swift
+++ IosBaseProject/Common/Base/IosBaseProjectApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import IQKeyboardManagerSwift
 
 @main
 struct IosBaseProjectApp: App {
@@ -47,3 +48,80 @@ extension IosBaseProjectApp {
 		UITabBar.appearance().scrollEdgeAppearance = appearance
 	}
 }
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+	// TODO: Enable push notification setup when FCM configuration is ready
+	// let authRepo = AuthenticationDefaultRepository()
+	
+	func application(
+		_ application: UIApplication,
+		didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+	) -> Bool {
+		IQKeyboardManager.shared.isEnabled = true
+		IQKeyboardManager.shared.resignOnTouchOutside = true
+		
+		// TODO: Enable push notification setup when FCM configuration is ready
+		// configurePushNotification()
+		
+		return true
+	}
+	
+	//	 TODO: Enable push notification setup when FCM configuration is ready
+	//	 private func configurePushNotification() {
+	//	     FirebaseApp.configure()
+	//	     Messaging.messaging().delegate = self
+	//	     UNUserNotificationCenter.current().delegate = self
+	//	     Task {
+	//	         try? await requestNotificationPermission()
+	//	     }
+	//	 }
+	//
+	//	func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+	//		Messaging.messaging().apnsToken = deviceToken
+	//	}
+	//
+	//	@MainActor
+	//	func requestNotificationPermission() async throws {
+	//		let granted = try await UNUserNotificationCenter.current()
+	//			.requestAuthorization(options: [.alert, .sound, .badge])
+	//
+	//		if granted {
+	//			UIApplication.shared.registerForRemoteNotifications()
+	//
+	//			guard let fcm = Messaging.messaging().fcmToken else { return }
+	//
+	//			authRepo.storeToken(from: fcm, with: KeychainKey.FCMToken.key)
+	//		}
+	//	}
+}
+
+// TODO: Enable push notification setup when FCM configuration is ready
+// extension AppDelegate: UNUserNotificationCenterDelegate, MessagingDelegate {
+//	func userNotificationCenter(
+//		_ center: UNUserNotificationCenter,
+//		willPresent notification: UNNotification,
+//		withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+//	) {
+//		completionHandler([.banner, .sound, .badge])
+//	}
+//
+//	func userNotificationCenter(
+//		_ center: UNUserNotificationCenter,
+//		didReceive response: UNNotificationResponse,
+//		withCompletionHandler completionHandler: @escaping () -> Void
+//	) {
+//		let userInfo = response.notification.request.content.userInfo
+//
+//		if let notification = userInfo["notification_id"] as? String {
+//			NotificationCenter.default.post(name: Notification.Name("NavigateFromPush"), object: nil, userInfo: ["notification_id": notification])
+//		}
+//
+//		completionHandler()
+//	}
+//
+//	func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+//		if let fcmToken {
+//			authRepo.storeToken(from: fcmToken, with: KeychainKey.FCMToken.key)
+//		}
+//	}
+//}
